5a9e55ef4033e4df2529dc2f47383f6a654fdce9
Kevin <ghekghek2@gmail.com>
2022-11-15 16:20:25 +0800

    add  navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
+import Navbar from "./components/navbar/Navbar.js";
 import Home from "./components/home/Home.js"
 import "./index.css"
 function App() {
   return (
-    <div className="App">
+    <div className="App section">
+  <Navbar/>
    <Home/>
+
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,35 @@
+import { useRef } from "react"
+import {FaBars,FaTimes} from "react-icons/fa"
+import React from 'react'
+import './navbar.css'
+function Navbar() {
+
+const navRef = useRef()
+
+const showNavbar = () =>{
+    navRef.current.classList.toggle("responsive-nav")
+}
+
+  return (
+    <header>
+        <h3>logo</h3>
+        <nav ref={navRef}>
+            <a href="/#">Home</a>
+            <a href="/#">About me</a>
+            <a href="/#">Skill</a>
+            <a href="/#">Project</a>
+            <a href="/#">Contact</a>
+          
+            <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+    <FaTimes/>
+            </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+    <FaBars/>
+            </button>
+        
+    </header>
+  )
+}
+
+export default Navbar
